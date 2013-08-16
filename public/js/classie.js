34181aa06d43464cd132aef561b803ce9bbb3e76
Brian Ortiz <ortzinator@gmail.com>
2013-08-15 22:11:49 -0400

    DRYing the js

--- public/js/classie.js
+++ public/js/classie.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
     $("input.focus").focus();
 
-    $("textarea.autoexpand").focus(function() {
-  		$(this).animate({width:"500px", height:"100px"},400);
+    var textAuto = $("textarea.autoexpand");
+    var textExpandSize = { width:"500px", height:"100px" };
+
+    textAuto.focus(function() {
+  		$(this).animate(textExpandSize, 400);
 	});
 
-	if ($("textarea.autoexpand").val() != '') {
-		$("textarea.autoexpand").css({width:"500px", height:"100px"});
+	if (textAuto.val() != '') {
+		textAuto.css(textExpandSize);
 	};
 
 	$('.markitup').markItUp(myMarkdownSettings);
 
-	$('input[type="submit"].disabled').attr('disabled','disabled');
+	$('input[type="submit"].disabled').attr('disabled', 'disabled');
 	$('textarea.disabled').attr('disabled','disabled');
 });
