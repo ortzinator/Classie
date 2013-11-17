a4667e4d09a9035290578ff034b7eef8961960ad
Brian Ortiz <ortzinator@gmail.com>
2013-11-17 00:00:51 -0500

    Changed login form style and added top margin to footer

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -2,24 +2,36 @@
 
 @section('content')
 
-{{ Form::open(['url' => 'auth/login']) }}
-
-<fieldset>
-	<legend>Log in</legend>
-	@if ($errors->any())
-		<div class="alert alert-error">Username or password incorrect.</div>
-	@endif
-	{{ Form::label('email', 'Email') }}
-	{{ Form::email('email', '', ['required' => '']) }}
-	{{ Form::label('password', 'Password') }}
-	{{ Form::password('password', ['required' => '']) }}
-	<label class="checkbox" for="remember">
-		{{ Form::checkbox('remember', 'remember') }} Remember
-	</label>
-	<div></div>
-	{{ Form::button('Submit', ['class' => 'btn primary', 'type' => 'submit']) }}
-</fieldset>
-
-{{ Form::close() }}
+<div class="row">
+	<div class="span12">
+		{{ Form::open(['url' => 'auth/login', 'class' => 'form-horizontal']) }}
+		<fieldset>
+			<div id="legend">
+				<legend class="">Login</legend>
+			</div>
+			<div class="control-group">
+				{{ Form::label('email', 'Email', ['class' => 'control-label']) }}
+				<div class="controls">
+					{{ Form::email('email', '', ['required' => '']) }}
+				</div>
+			</div>
+			<div class="control-group">
+				{{ Form::label('password', 'Password', ['class' => 'control-label']) }}
+				<div class="controls">
+					{{ Form::password('password', ['required' => '']) }}
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="controls">
+					<label class="checkbox" for="remember" class="control-label">
+						{{ Form::checkbox('remember', 'remember') }} Remember
+					</label>
+					<button class="btn btn-success">Login</button>
+				</div>
+			</div>
+		</fieldset>
+		{{ Form::close() }}
+	</div>
+</div>
 
 @stop
