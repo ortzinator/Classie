4f3cb5b3f42ba47cac2484f177bc0715882a93d5
Brian Ortiz <ortzinator@gmail.com>
2013-12-03 13:05:41 -0500

    Removed unused use statements.

--- app/lib/Ortzinator/Classie/Console/InstallCommand.php
+++ app/lib/Ortzinator/Classie/Console/InstallCommand.php
@@ -1,8 +1,6 @@
 <?php namespace Ortzinator\Classie\Console;
 
 use Illuminate\Console\Command;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Config\Repository as Config;
 
