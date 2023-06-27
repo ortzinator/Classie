b5a3270f5bae5f63ab362daa8008c9b288889d48
Ortzinator <ortzinator@gmail.com>
2023-06-27 13:43:19 -0400

    Visual tweaks

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="/posts" method="POST" role="form">
-        {{ csrf_field() }}
+    <div class="container">
+        <form action="/posts" method="POST" role="form">
+            {{ csrf_field() }}
 
-        <legend>Create Post</legend>
+            <legend>Create Post</legend>
 
-        @if($errors->count() > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+            @if($errors->count() > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <div class="mb-3">
+                <label class="form-label" for="title">Title:</label>
+                <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
             </div>
-        @endif
 
-        <div class="form-group">
-            <label for="title">Title:</label>
-            <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
-        </div>
+            <div class="mb-3">
+                <label class="form-label" for="body">Body:</label>
+                <textarea name="body" id="body" class="form-control" rows="3">{{ old('body') }}</textarea>
+            </div>
 
-        <div class="form-group">
-            <label for="body">Body</label>
-            <textarea type="text" name="body" id="body" class="form-control">
-                {{ old('body') }}
-            </textarea>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
