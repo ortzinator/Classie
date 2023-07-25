58caee92cd20abf48ee0ad666acc148a4f554406
Ortzinator <ortzinator@gmail.com>
2023-07-25 12:52:57 -0400

    Fix error when no images on a post

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.app')
 
+
 @section('content')
-    <div class="container" x-data="{ currentImage: '{{ asset('storage/images/' . $post['images'][0]['file']) }}'}">
+    <div class="container">
         <h1>{{ $post->title }}</h1>
-        <a :href="currentImage" target="_blank"><img :src="currentImage" alt="" height="400"></a>
-        <div class="row">
-            @foreach($post['images'] as $image)
-                <div class="col col-sm-1">
-                    <a href="#" @click.prevent="currentImage = '{{ asset('storage/images/' . $image['file']) }}'">
-                        <img src="{{ asset('storage/images/th_' . $image['file']) }}" class="img-thumbnail">
-                    </a>
+        @if($post['images']->isNotEmpty())
+            <div x-data="{ currentImage: '{{ asset('storage/images/' . $post['images'][0]['file']) }}'}">
+                <a :href="currentImage" target="_blank"><img :src="currentImage" alt="" height="400"></a>
+                <div class="row">
+                    @foreach($post['images'] as $image)
+                        <div class="col col-sm-1">
+                            <a href="#"
+                               @click.prevent="currentImage = '{{ asset('storage/images/' . $image['file']) }}'">
+                                <img src="{{ asset('storage/images/th_' . $image['file']) }}" class="img-thumbnail">
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endif
         <div>{!! nl2br(e($post->body)) !!}</div>
     </div>
 @endsection
